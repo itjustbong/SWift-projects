3cb8be0612b36ab48276cef33298dad29a747ba6
Bong <qhdgkdbs@gmail.com>
2023-03-02 02:09:04 -0800

    Feat: Dice 프로젝트 완성

--- Dicees/Dicee-iOS13/ViewController.swift
+++ Dicees/Dicee-iOS13/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // let is CONST
+        // var is VARIABLE
+        let dice = [#imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ]
+        
+        // randomElement === Int.random(in: 0...5)
+        diceImageView1.image = dice.randomElement()
+        diceImageView2.image = dice[Int.random(in: 0...5)]
     }
-
-
+    
 }
 
---
+++ keyboard/keyboard/keyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  keyboardApp.swift
+//  keyboard
+//
+//  Created by bong on 2023/03/01.
+//
+
+import SwiftUI
+
+@main
+struct keyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
